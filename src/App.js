1045273c38f8054646b796bfdc05fc11d0ba468e
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-30 20:41:07 -0600

    Make data flow notes in app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 
 class App extends Component {
 
+// if doing multiple locations, take out of componentDidMount and on a button click to move from home page to trails page
+// if ^ interpolate fetch with diff coordinates?
+// split up data into two fetches and clean data twice?
+// move App to either components or container 
+
   async componentDidMount() {
     try {
       const trails = await fetchData('https://www.mtbproject.com/data/get-trails?lat=39.7392&lon=-104.9903&maxDistance=10&key=200628346-0f130fc8870531d529e09b85e721317a')
