796224414ec8e19717a217026c97f1de18b05abe
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-31 21:58:36 -0600

    Add image alt tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import logo from '../../images/gravity_logo.svg'
 export const Header = () => {
   return (
     <header className="Header">
-      <img className="Header-img" src={logo} />
+      <img className="Header-img" src={logo} alt="Gravity logo" />
       <h2 className="Header-h2">SHUTTLE SERVICE</h2>
     </header>
   )
