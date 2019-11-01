8adcb7042b1237ac78c6afabcfa3c34b4fa28a94
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-01 07:54:45 -0600

    Style cards

--- src/containers/Trail/Trail.js
+++ src/containers/Trail/Trail.js
@@ -4,8 +4,8 @@ import './Trail.css';
 export const Trail = ({ name, stars }) => {
   return (
     <section className="Trail">
-      <p>{name}</p>
-      <p>{stars}</p>
+      <p className="Trail-p">{stars} Stars</p>
+      <p className="Trail-p">{name}</p>
     </section>
   )
 }
