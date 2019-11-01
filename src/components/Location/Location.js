d7669dee2db82e8e30f960c8b44c21689561aeb9
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-31 22:22:12 -0600

    Route location buttons to trails route

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Location.css';
 
 export const Location = () => {
   return (
     <section className="Location">
       <h4 className="Location-h4">CHOOSE LOCATION</h4>
-      <button className="Location-btn">DENVER</button>
-      <button className="Location-btn">BOULDER</button>
+      <Link to ="/trails"><button className="Location-btn">DENVER</button></Link>
+      <Link to ="/trails"><button className="Location-btn">BOULDER</button></Link>
     </section>
   )
 }
