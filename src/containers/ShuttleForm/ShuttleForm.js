50d4eff6c1dc0755d8530ebf547796d028175190
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-01 10:23:04 -0600

    Allow input values to update state

--- src/containers/ShuttleForm/ShuttleForm.js
+++ src/containers/ShuttleForm/ShuttleForm.js
@@ -4,15 +4,45 @@ import './ShuttleForm.css';
 export class ShuttleForm extends Component {
   constructor() {
     super();
-    state: {
-
+    this.state = {
+      name: '',
+      email: ''
     }
   }
+
+  clearInputs = () => {
+    this.setState({
+      name: '',
+      email: ''
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  // submitBooking = () => {
+  //  should be an action that puts this info into the store
+  // }
 
   render() {
     return (
       <form className="ShuttleForm">
-        form
+        <input className="ShuttleForm-input"
+          name="name"
+          type="text"
+          placeholder="Enter Name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <input className="ShuttleForm-input"
+          name="email"
+          type="text"
+          placeholder="Enter Email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <button className="ShuttleForm-btn">SUBMIT BOOKING</button>
       </form>
     )
   }
