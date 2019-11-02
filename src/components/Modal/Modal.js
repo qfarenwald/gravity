f2227f5bf33324e18e25636c35f53cec8ba8a409
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-02 12:19:31 -0600

    Add email message to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,6 +6,7 @@ export const Modal = () => {
   return (
     <section className="Modal">
       <h3>YOUR SHUTTLE IS BOOKED</h3>
+      <h3>A CONFIRMATION EMAIL HAS BEEN SENT</h3>
       <h3>GET READY TO DROP IN!</h3>
       <Link to='/trails' className="link"><h5>BACK TO TRAILS</h5></Link>
     </section>
