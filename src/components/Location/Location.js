0fc5c71a22260893941ce1c6afb5cde3d06e723c
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-01 18:06:10 -0600

    Edit sub heading spacing

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -5,9 +5,11 @@ import './Location.css';
 export const Location = () => {
   return (
     <section className="Location">
-      <h4 className="Location-h4">CHOOSE LOCATION</h4>
+      <h3>CHOOSE LOCATION</h3>
       <Link to ="/trails"><button className="Location-btn">DENVER</button></Link>
       <Link to ="/trails"><button className="Location-btn">BOULDER</button></Link>
     </section>
   )
 }
+
+// className="Location-h4"
