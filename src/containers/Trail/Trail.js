e22de67aef47d12ae6110635904c0c65b9161574
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-02 13:52:39 -0600

    Add data and edit css

--- src/containers/Trail/Trail.js
+++ src/containers/Trail/Trail.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Trail.css';
 
-export const Trail = ({ name, stars }) => {
+export const Trail = ({ name, stars, descent }) => {
   return (
     <section className="Trail">
       <p className="Trail-p stars">{stars} Stars</p>
       <p className="Trail-p name">{name}</p>
+      <p className="Trail-p descent">{descent}ft Descent</p>
       <Link to="/shuttle"><button className="Trail-btn">BOOK</button></Link>
     </section>
   )
