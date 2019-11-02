9e16c474d7fbb7e8324b9dee66f11239e155ec13
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-02 08:18:26 -0600

    Utilize redirect to route from shuttle booking to confirmation message

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Modal.css';
 
 export const Modal = () => {
   return (
     <section className="Modal">
-      modal
+      <h3>YOUR SHUTTLE IS BOOKED</h3>
+      <h3>GET READY TO DROP IN!</h3>
+      <Link to='/trails' className="link"><h5>BACK TO TRAILS</h5></Link>
     </section>
   )
 }
