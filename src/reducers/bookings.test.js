78da092692c029588231b126a4cafd4ea1ade1af
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-02 23:54:41 -0600

    Write bookings reducer tests for initial state and adding a booking to state

---
+++ src/reducers/bookings.test.js
@@ -0,0 +1,38 @@
+import { bookings } from '../reducers/bookings';
+
+describe('bookings', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+
+    const result = bookings(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('add booking to state', () => {
+    const initialState = [];
+    const mockNewBooking = {
+      name: "Quinne Farenwald",
+      email: "q.farenwald@gmail.com",
+      riders: "4",
+      bikes: "4",
+      date: "1211",
+      starttime: "1030",
+      endtime: "6",
+      formReady: true,
+      redirect:false
+    }
+    const state = initialState;
+    const action = {
+      type: 'MAKE_BOOKING',
+      booking: mockNewBooking
+    }
+
+    const newState = [...initialState, mockNewBooking];
+
+    const result = bookings(state, action);
+
+    expect(result).toEqual(newState);
+  });
+
+});
