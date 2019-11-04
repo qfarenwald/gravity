40c6514b24cc56c1be31c05dc01248fb34dc2e23
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-04 09:25:51 -0700

    Updates

---
+++ precache-manifest.76d8c1e360ce105256932a12b2059a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b022057bc65a702ba96ef34da3c533",
+    "url": "/gravity/index.html"
+  },
+  {
+    "revision": "2d30e6e4044b3f4d2ecf",
+    "url": "/gravity/static/css/main.44d06767.chunk.css"
+  },
+  {
+    "revision": "71e4903b2c3f7114d40b",
+    "url": "/gravity/static/js/2.b6805061.chunk.js"
+  },
+  {
+    "revision": "2d30e6e4044b3f4d2ecf",
+    "url": "/gravity/static/js/main.46ed9702.chunk.js"
+  },
+  {
+    "revision": "e6ffd06d3fe9df681bc5",
+    "url": "/gravity/static/js/runtime-main.71074856.js"
+  },
+  {
+    "revision": "1064b9f7a8a2f64fcb20770466eabd27",
+    "url": "/gravity/static/media/gravity_bkgd.1064b9f7.png"
+  },
+  {
+    "revision": "f38521b77bd53516329f4557aafbf8d8",
+    "url": "/gravity/static/media/gravity_logo.f38521b7.svg"
+  }
+]);
